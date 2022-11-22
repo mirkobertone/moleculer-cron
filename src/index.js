bc3df26988bfd9955b3ca7d037a62eef49f25d7c
Mirko Bertone <bertonemirko@gmail.com>
2022-11-22 19:47:17 +0100

    update peerDep

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@
  * moleculer-cron
  */
 
- "use strict";
+"use strict";
 
 const cron = require("cron");
-
+const { v4: uuid } = require("uuid");
 
 /**
 	cronOpts
@@ -23,108 +23,91 @@ const cron = require("cron");
 */
 
 /**
-*  Mixin service for Cron
-*
-* @name moleculer-cron
-* @module Service
-*/
+ *  Mixin service for Cron
+ *
+ * @name moleculer-cron
+ * @module Service
+ */
 module.exports = {
-	name: "cron",
+  name: "cron",
 
-	/**
-	 * Methods
-	 */
-	methods: {
+  /**
+   * Methods
+   */
+  methods: {
+    /**
+     * Find a job by name
+     *
+     * @param {String} name
+     * @returns {CronJob}
+     */
+    getJob(name) {
+      return this.$crons.find(
+        (job) => job.hasOwnProperty("name") && job.name == name
+      );
+    },
 
-		/**
-		 * Find a job by name
-		 * 
-		 * @param {String} name 
-		 * @returns {CronJob}
-		 */
-		getJob(name) {
-			return this.$crons.find((job) => job.hasOwnProperty("name") && job.name == name);
-		},
+    /**
+     * Get a Cron time
+     * @param {String} time
+     */
+    getCronTime(time) {
+      return new cron.CronTime(time);
+    },
+  },
 
-		//	stolen on StackOverflow
-		makeid(size) {
-			var text = "";
-			var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  /**
+   * Service created lifecycle event handler
+   */
+  created() {
+    this.$crons = [];
 
-			for (var i = 0; i < size; i++)
-			text += possible.charAt(Math.floor(Math.random() * possible.length));
+    if (this.schema.crons) {
+      this.$crons = this.schema.crons.map((job) => {
+        //	Prevent error on runOnInit that handle onTick at the end of the constructor
+        //	We handle it ourself
+        const cacheFunction = job.runOnInit;
+        job.runOnInit = undefined;
+        //	Just add the broker to handle actions and methods from other services
+        const instance_job = new cron.CronJob(
+          Object.assign(job, {
+            context: Object.assign(this.broker, {
+              getJob: this.getJob,
+            }),
+          })
+        );
+        instance_job.runOnStarted = cacheFunction;
+        instance_job.manualStart = job.manualStart || false;
+        instance_job.name = job.name || uuid();
 
-			return text;
-		},
+        return instance_job;
+      });
+    }
 
-		/**
-		 * Get a Cron time
-		 * @param {String} time 
-		 */
-		getCronTime(time) {
-			return new cron.CronTime(time);
-		}
+    return this.Promise.resolve();
+  },
 
-	},
+  /**
+   * Service started lifecycle event handler
+   */
+  started() {
+    this.$crons.map((job) => {
+      if (!job.manualStart) {
+        job.start();
+      }
+      this.logger.info("Start Cron - ", job.name);
+      if (job.runOnStarted) {
+        job.runOnStarted();
+      }
+    });
+  },
 
-	/**
-	 * Service created lifecycle event handler
-	 */
-	created() {
-		this.$crons = [];
-
-		if (this.schema.crons) {
-			this.$crons = this.schema.crons.map((job) => {
-				//	Prevent error on runOnInit that handle onTick at the end of the constructor
-				//	We handle it ourself
-				var cacheFunction = job.runOnInit;
-				job.runOnInit = undefined;
-				//	Just add the broker to handle actions and methods from other services
-				var instance_job = new cron.CronJob(
-					Object.assign(
-						job,
-						{
-							context: Object.assign(
-											this.broker,
-											{
-												getJob: this.getJob
-											}
-									)
-						}
-					)
-				);
-				instance_job.runOnStarted = cacheFunction;
-				instance_job.manualStart = job.manualStart || false;
-				instance_job.name = job.name || this.makeid(20);
-				
-				return instance_job;
-			});
-		}
-
-		return this.Promise.resolve();
-	},
-
-	/**
-	 * Service started lifecycle event handler
-	 */
-	started() {
-		this.$crons.map((job) => {
-			if (!job.manualStart) {
-				job.start();
-			}
-			this.logger.info("Start Cron - ", job.name);
-			if (job.runOnStarted) {
-				job.runOnStarted();
-			}
-		});
-	},
-
-	/**
-	 * Service stopped lifecycle event handler
-	 */
-	stopped() {
-		this.$crons.map((job) => {
-			job.stop();
-		});
-	}
+  /**
+   * Service stopped lifecycle event handler
+   */
+  stopped() {
+    this.$crons.map((job) => {
+      job.stop();
+    });
+  },
 };
